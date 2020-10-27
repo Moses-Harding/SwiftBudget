2ca2c58044fcb0a7a7f751404b7fe2c835c6186b
Moses Harding <moseshh@gmail.com>
2020-10-27 11:23:48 -0400

    Initial Commit

---
+++ SwiftBudget/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftBudget
+//
+//  Created by Moses Harding on 10/27/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
